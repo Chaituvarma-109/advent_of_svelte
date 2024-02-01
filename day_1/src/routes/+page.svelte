09b61dbe5cd163778731d79c93f64fcc79ba18e1
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2024-02-01 21:50:13 +0530

    day-1 added tailwindcss

--- day_1/src/routes/+page.svelte
+++ day_1/src/routes/+page.svelte
@@ -3,47 +3,56 @@
 
     export let data: PageData
 
-    let pname = '';
-    let ptally: number;
+    let pname = 'johnny';
+    let ptally: number = 0;
 
     $: ({ letter } = data)
 </script>
 
-<h1>Letters</h1>
 
-<input type="text" bind:value={pname}>
-<input type="number" bind:value={ptally} />
+<div class="flex justify-center mb-10">
+    <h1 class="text-lg font-medium">Letters</h1>
+</div>
 
-<table>
-    <tr>
-        <th>name</th>
-        <th>tally</th>
-        <th>NaughtyorNice</th>
-    </tr>
-    <tr>
-        <td>{pname}</td>
-        <td>{ptally}</td>
-        <td>{ptally > 0 ? 'Nice' : 'Naughty'}</td>
-    </tr>
-</table>
+<div class="flex m-10">
+    <div class="overflow-x-auto basis-1/2">
+        <table class="table-md">
+            <tr>
+                <th>Name</th>
+                <th>tally</th>
+                <th>NaughtyorNice</th>
+            </tr>
+            {#each letter as {name, tally} }
+                <tr>
+                    <td>{name}</td>
+                    <td>{tally}</td>
+                    <td>{tally > 0 ? 'Nice' : 'Naughty'}</td>
+                </tr>
+            {/each}
+        </table>
+    </div>
 
-<table>
-    <tr>
-        <th>name</th>
-        <th>tally</th>
-        <th>NaughtyorNice</th>
-    </tr>
-    {#each letter as {name, tally} }
-        <tr>
-            <td>{name}</td>
-            <td>{tally}</td>
-            <td>{tally > 0 ? 'Nice' : 'Naughty'}</td>
-        </tr>
-    {/each}
-</table>
+    <div class="divider divider-horizontal"></div>
 
-<style>
-    table, th, td {
-        border:1px solid black;
-    }
-</style>
+    <div class="flex flex-col basis-1/2">
+        <div class="flex">
+            <input type="text" bind:value={pname} class="input input-bordered w-full max-w-xs">
+            <input type="number" bind:value={ptally} class="input input-bordered w-full max-w-xs" />
+        </div>
+
+        <div class="overflow-x-auto">
+            <table class="table-md">
+                <tr>
+                    <th>name</th>
+                    <th>tally</th>
+                    <th>NaughtyorNice</th>
+                </tr>
+                <tr>
+                    <td>{pname}</td>
+                    <td>{ptally}</td>
+                    <td>{ptally > 0 ? 'Nice' : 'Naughty'}</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
