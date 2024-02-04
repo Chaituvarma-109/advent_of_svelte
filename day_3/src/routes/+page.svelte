25b995bb51159f1242555d10891e42af2372579a
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2024-02-04 13:00:53 +0530

    added beer css

--- day_3/src/routes/+page.svelte
+++ day_3/src/routes/+page.svelte
@@ -7,45 +7,47 @@
     $: ({elfPresents} = data)
 </script>
 
-<main class="container">
-    <article>
-        <h1>Current Sleigh Weight {totalWeight.toFixed(2)}</h1>
-
-        {#if totalWeight === 0}
-            <p>No presents</p>
-        {:else if totalWeight > 0 && totalWeight < 99}
-            <p>Add some Presents</p>
-        {:else if totalWeight >= 99 && totalWeight <= 100}
-            <p>You are good to go</p>
-        {:else if totalWeight > 100}
-            <p>Exceeding the maximum weight, please remove some presents</p>
-        {/if}
-    </article>
-    <table role="grid">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Weight</th>
-                <th scope="col">Add to SLB</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each elfPresents as {name, weight}, i}
-                <tr>
-                    <th scope="row">{i+1}</th>
-                    <td>{name}</td>
-                    <td>{weight}</td>
-                    <td><button class="btn" on:click={() => totalWeight += weight}>+</button></td>
-                </tr>
-            {/each}
-        </tbody>
-    </table>
+<main class="responsive max">
+    <div class="grid">
+        <div class="s6">
+            <article class="no-padding round border small tertiary-container center-align middle-align">
+                <div class="padding absolute center middle">
+                    <h5>Current Sleigh Weight {totalWeight.toFixed(2)}</h5>
+                    {#if totalWeight === 0}
+                        <p>No presents</p>
+                    {:else if totalWeight > 0 && totalWeight < 99}
+                        <p>Add some Presents</p>
+                    {:else if totalWeight >= 99 && totalWeight <= 100}
+                        <p>You are good to go</p>
+                    {:else if totalWeight > 100}
+                        <p>Exceeding the maximum weight, please remove some presents</p>
+                    {/if}
+                </div>
+            </article>
+        </div>
+        <div class="s6">
+            <div class="large-height scroll">
+                <table class="border medium-space center-align">
+                    <thead class="fixed">
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Weight</th>
+                            <th>Add to SLB</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {#each elfPresents as {name, weight}, i}
+                            <tr>
+                                <th>{i+1}</th>
+                                <td>{name}</td>
+                                <td>{weight}</td>
+                                <td><button class="btn" on:click={() => totalWeight += weight}>+</button></td>
+                            </tr>
+                        {/each}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </main>
-
-<style>
-    .btn {
-        width: 25%;
-        border: 50px;
-    }
-</style>
